df1ed3ed2ba54cfff74c09bd61471f54dc01ead6
Adam Crosby <adam@uptill3.com>
2018-08-09 18:10:43 -0400

    python3 enabled

--- xccdf-xml2tsv.py
+++ xccdf-xml2tsv.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ###
 # (C) 2010 Adam Crosby
 # Licensed under:
 #  http://creativecommons.org/licenses/by-sa/3.0/
 ##
+import importlib
 import csv
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+#importlib.reload(sys)
+#sys.setdefaultencoding('utf-8')
 import xml.etree.ElementTree as ET
 xmlns = "http://checklists.nist.gov/xccdf/1.1"
 
 if len(sys.argv) != 2:
-	print "  XCCDF-xml2tsv converts XCCDF XML documents (such as DISA STIGs)"
-	print "    into easier to use Tab-Separated documents."
-	print "  Please run as '%s' <filename> and redirect output as needed." % sys.argv[0]
-	print "  Files should open easily in Excel."
-	print "  E.g.:\n\t %s U_Perimeter_Router_v8R2_manual.xccdf.xml > output.tsv" % sys.argv[0]
+	print ("  XCCDF-xml2tsv converts XCCDF XML documents (such as DISA STIGs)")
+	print ("    into easier to use Tab-Separated documents.")
+	print ("  Please run as '%s' <filename> and redirect output as needed." % sys.argv[0])
+	print ("  Files should open easily in Excel.")
+	print ("  E.g.:\n\t %s U_Perimeter_Router_v8R2_manual.xccdf.xml > output.tsv" % sys.argv[0])
 	sys.exit(0)
 try:
 	xml = ET.parse(sys.argv[1])
-except Exception,e:
-	print "Error, unable to parse XML document.  Are you sure that's XCCDF?"
+except Exception:
+	print ("Error, unable to parse XML document.  Are you sure that's XCCDF?")
 	sys.exit(-1)
 
 benchmark = xml.getroot()
@@ -41,7 +42,7 @@ groups = benchmark.findall("{%s}Group" % xmlns)
 
 
 
-csvfile = open('tmp.csv', 'wb')
+csvfile = open('tmp.csv', 'w')
 output = csv.writer(csvfile, dialect='excel')
 output.writerow(('STIG ID', 'Version', 'Rule Title', 'Title', 'Severity', 'Check Text', 'Fix Text', 'CCI'))
 for group in groups:
